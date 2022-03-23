f6362253563d6d3d363bbc18ea29fa31fd356588
federico.correa <fedecor27@gmail.com>
2022-03-23 10:05:04 -0300

    change id to String

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const uniqid = require("uniqid");
 app.use(express.json());
 app.use(cors());
 
 let notes = [
   {
-    id: 1,
+    id: uniqid(),
     tittle: "Note test 1",
     text: "Note test 1",
     date: "Sat May   1 2022",
     color: "dark",
   },
   {
-    id: 2,
+    id: uniqid(),
     tittle: "Note test 2",
     text: "Note test 2",
     date: "Sun Feb 19 2022",
     color: "light",
   },
   {
-    id: 3,
+    id: uniqid(),
     tittle: "Note test 3",
     text: "Note test 3",
     date: "Sat Mar 29 2021",
@@ -30,10 +30,8 @@ let notes = [
 ];
 
 const createNewNote = (note) => {
-  const ids = notes.map((elem) => elem.id);
-  //const maxId = Math.max(...ids);
   return {
-    id: note.id,
+    id: uniqid(),
     tittle: note.tittle,
     text: note.content,
     date: new Date().toDateString(),
@@ -43,7 +41,7 @@ const createNewNote = (note) => {
 
 //sends json with all notes
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  return response.json(notes);
 });
 
 //sends note with especific id
@@ -51,20 +49,20 @@ app.get("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   const note = notes.find((elem) => elem.id === id);
   if (!note) response.status(404).end();
-  response.json(note);
+  return response.json(note);
 });
 
 //adds note
 app.post("/api/notes", (request, response) => {
   const note = request.body;
-  if (!note || !note.content) {
+  if (!note || !note.text) {
     return response.status(400).json({
       error: "content missing",
     });
   }
   const newnote = createNewNote(note);
   notes = [...notes, newnote];
-  response.json(newnote);
+  return response.json(newnote);
 });
 
 //deletes note with especific id
@@ -72,7 +70,7 @@ app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   notes = notes = notes.filter((elems) => elems.id !== id);
   response.status(204);
-  response.json(notes);
+  return response.json(notes);
 });
 
 const PORT = process.env.PORT || 3005;
